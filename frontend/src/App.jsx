0b4190f05b051b63a8c00bfb1765590faff83fc8
tulik <tolqinmirsaliyev@gmail.com>
2024-08-16 13:03:52 -0500

    ddd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register'
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/menu" element={<Menu />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
