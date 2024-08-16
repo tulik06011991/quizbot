b96c17168facfbcea26552f7e2e62cf5f171225f
tulik <tolqinmirsaliyev@gmail.com>
2024-08-16 12:58:20 -0500

    hhhh

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -4,6 +4,7 @@ const Menu = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
  
+
     <main className="flex-grow flex flex-col justify-center items-center text-center">
       <h2 className="text-4xl font-bold text-gray-800 mb-4">Welcome to the Quiz Bot</h2>
       <p className="text-gray-600 mb-8">Test your knowledge and improve your skills</p>
