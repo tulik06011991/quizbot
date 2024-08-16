fb2a3a3fafd9dcead216d95deaf7120dd2100206
tulik <tolqinmirsaliyev@gmail.com>
2024-08-16 13:27:51 -0500

    gfdgfg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Register from './components/Register'
+import NotFound from './components/NotFound'; 
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<NotFound />} /> {/* 404 sahifasini qo'shish */}
       </Routes>
       <Footer />
     </Router>
