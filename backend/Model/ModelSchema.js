816d832905386c3e537ad69f5945193f2124d838
realProject <tolqinmirsaliyev@gmail.com>
2024-08-17 10:46:13 +0530

    ccvc

--- backend/Model/ModelSchema.js
+++ backend/Model/ModelSchema.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: 'user' } // Admin yoki user rollari
+  role: { type: Boolean, default: false } // Admin yoki user rollari
 });
 
 const User = mongoose.model('User', userSchema);
