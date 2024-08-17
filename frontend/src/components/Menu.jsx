d29b32a8359284209e6ab07ff91094927a6cf384
realProject <tolqinmirsaliyev@gmail.com>
2024-08-17 11:07:46 +0530

    dsdfsds

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -9,8 +9,9 @@ const Menu = () => {
     <main className="flex-grow flex flex-col justify-center items-center text-center">
       <h2 className="text-4xl font-bold text-gray-800 mb-4">Welcome to the Quiz Bot</h2>
       <p className="text-gray-600 mb-8">Test your knowledge and improve your skills</p>
-      <button className="px-6 py-3 bg-blue-500 text-white text-lg rounded-full shadow-lg hover:bg-blue-400 transition duration-300">
-        Start Quiz
+      
+      <button >
+         <a href="/test" className="px-6 py-3 bg-blue-500 text-white text-lg rounded-full shadow-lg hover:bg-blue-400 transition duration-300" >Start Quiz</a>
       </button>
     </main>
 
