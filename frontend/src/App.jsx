c282f37b9d53125502f8a77c89559979d8721cfe
realProject <tolqinmirsaliyev@gmail.com>
2024-08-21 09:41:14 +0530

    xcxcxc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,73 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './components/Login';
-import Register from './components/Register'
+import Register from './components/Register';
 import NotFound from './components/NotFound'; 
 import Test from './components/Test';
 import Word from './components/Word';
 import Fanlar from './components/Fanlar';
 import FanlarOquvchi from './components/FanlarOquchi'
 
-
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token'); // Tokenni olish
+  return token ? children : <Navigate to="/" replace />; // Token bo'lmasa login sahifasiga yo'naltirish
+};
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/word" element={<Word />} />
-        <Route path="/fanlar" element={<Fanlar />} />
-        <Route path="/fanlaroquvchi" element={<FanlarOquvchi />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/menu"
+          element={
+            <PrivateRoute>
+              <Menu />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/test"
+          element={
+            <PrivateRoute>
+              <Test />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/word"
+          element={
+            <PrivateRoute>
+              <Word />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/fanlar"
+          element={
+            <PrivateRoute>
+              <Fanlar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/fanlaroquvchi"
+          element={
+            <PrivateRoute>
+              <FanlarOquvchi />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} /> {/* 404 sahifasini qo'shish */}
       </Routes>
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
