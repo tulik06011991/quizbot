05d150c742eddaf9613d5e624d6d8dc098577a68
realProject <tolqinmirsaliyev@gmail.com>
2024-08-21 10:44:35 +0530

    cxcxcx

--- backend/controller/javob.js
+++ backend/controller/javob.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const CorrectAnswer = require('../Model/natijalar');
+const CorrectAnswer = require('../Model/togri'); // Natijalar model
+const User = require('../Model/ModelSchema'); // User modeli
 
 // Test yakunlash va natijani hisoblash
 const finishQuiz = async (req, res) => {
-  const { answers } = req.body; // Foydalanuvchidan yuborilgan javoblar
+  const { userId, answers } = req.body; // Foydalanuvchi ID'si va javoblar
   let correctCount = 0;
   const totalQuestions = Object.keys(answers).length;
 
   try {
+    // Foydalanuvchi ID'sini tekshirish
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'Foydalanuvchi topilmadi' });
+    }
+
     // Har bir savol uchun to'g'ri javobni tekshirish
     for (const [questionId, selectedOptionId] of Object.entries(answers)) {
       const correctAnswer = await CorrectAnswer.findOne({ questionId }).populate('correctOptionId');
-      
+
       if (correctAnswer && correctAnswer.correctOptionId._id.toString() === selectedOptionId) {
         correctCount++;
       }
