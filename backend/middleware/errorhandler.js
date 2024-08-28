3808397477845b844249f8e910966de20d8fbe90
realProject <tolqinmirsaliyev@gmail.com>
2024-08-28 09:21:38 +0530

    vcvcvc

--- backend/middleware/errorhandler.js
+++ backend/middleware/errorhandler.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1]; // Bearer so'zidan keyingi haqiqiy tokenni olish
 // Tokenni frontenddan headers orqali olish
-console.log(token)
+// console.log(token)
 
   if (!token) {
     return res.status(403).json({ message: 'No token provided' }); // Token bo'lmasa xatolik berish
